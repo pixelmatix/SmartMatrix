e267167c4e7657cfbf98074bf4baedf571244616
Louis Beaudoin <louis@embedded-creations.com>
2015-09-10 10:04:13 -0400

    swap order of width and height in init block

--- examples/SpectrumAnalyzer/SpectrumAnalyzer.ino
+++ examples/SpectrumAnalyzer/SpectrumAnalyzer.ino
@@ -32,8 +32,8 @@
 #include <FastLED.h>
 
 #define COLOR_DEPTH 24                  // known working: 24, 48 - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24
-const uint8_t kMatrixHeight = 32;       // known working: 32, 64, 96, 128
 const uint8_t kMatrixWidth = 32;        // known working: 16, 32, 48, 64
+const uint8_t kMatrixHeight = 32;       // known working: 32, 64, 96, 128
 const uint8_t kRefreshDepth = 36;       // known working: 24, 36, 48
 const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save memory, more to keep from dropping frames and automatically lowering refresh rate
 const uint8_t kPanelType = SMARTMATRIX_HUB75_32ROW_MOD16SCAN;   // use SMARTMATRIX_HUB75_16ROW_MOD8SCAN for common 16x32 panels
