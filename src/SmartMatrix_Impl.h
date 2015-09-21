ba2de6583bb9d39fcd77c0f465d463e3aa1523e5
Louis Beaudoin <louis@embedded-creations.com>
2015-09-21 15:20:32 -0400

    support compiling with a non USB-serial configuration

--- src/SmartMatrix_Impl.h
+++ src/SmartMatrix_Impl.h
@@ -185,11 +185,12 @@ void SmartMatrix3<refreshDepth, matrixWidth, matrixHeight, panelType, optionFlag
 
   loops++;
   if(currentMillis - lastMillis >= 1000){
+#if defined(USB_SERIAL)
     if(Serial) {
         Serial.print("Loops last second:");
         Serial.println(loops);
     }
-    
+#endif    
     lastMillis = currentMillis;
     loops = 0;
   }
