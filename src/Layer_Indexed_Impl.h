028b3b3881dfa4d8b8fd8a2cdbb73e30cf903abc
Louis Beaudoin <louis@embedded-creations.com>
2018-06-21 12:55:01 +0100

    (mostly ESP32) - Fix asserts that were doing nothing, make sure any malloc that fails isn't overlooked

--- src/Layer_Indexed_Impl.h
+++ src/Layer_Indexed_Impl.h
@@ -42,7 +42,11 @@ template <typename RGB, unsigned int optionFlags>
 SMLayerIndexed<RGB, optionFlags>::SMLayerIndexed(uint16_t width, uint16_t height) {
     // size of bitmap is 2 * INDEXED_BUFFER_SIZE
     indexedBitmap = (uint8_t*)malloc(2 * width * (height / 8));
+#ifdef ESP32
+    assert(indexedBitmap != NULL);
+#else
     this->assert(indexedBitmap != NULL);
+#endif
     memset(indexedBitmap, 0x00, 2 * width * (height / 8));
     this->matrixWidth = width;
     this->matrixHeight = height;
