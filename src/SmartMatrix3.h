2f15f5df524e3907eebadd08dfe42263abb4ab60
Louis Beaudoin <louis@embedded-creations.com>
2020-05-25 23:57:25 +0100

    Refactor SmartMatrix3.h to use common headers and definitions as much as possible, and include platform-specific only where needed

--- src/SmartMatrix3.h
+++ src/SmartMatrix3.h
@@ -28,10 +28,6 @@
 
 #include "Arduino.h"
 
-#if defined(__IMXRT1062__) // Teensy 4.0/4.1
-
-#include "MatrixHardware_KitV4T4.h"
-
 #include "MatrixCommon.h"
 #include "CircularBuffer_SM.h"
 
@@ -41,53 +37,19 @@
 
 #include "SmartMatrixMultiplexedCommon.h"
 
-#include "SmartMatrixRefreshT4.h"
-#include "SmartMatrixCalcT4.h"
 #include "SmartMatrixAPA102Refresh.h"
 #include "SmartMatrixAPA102Calc.h"
 
-#define SMARTMATRIX_ALLOCATE_BUFFERS(matrix_name, width, height, pwm_depth, buffer_rows, panel_type, option_flags) \
-  static volatile DMAMEM SmartMatrixRefreshT4<pwm_depth, width, height, panel_type, option_flags>::rowDataStruct rowsDataBuffer[buffer_rows]; \
-  SmartMatrixRefreshT4<pwm_depth, width, height, panel_type, option_flags> matrix_name##Refresh(buffer_rows, rowsDataBuffer); \
-  SmartMatrix3<pwm_depth, width, height, panel_type, option_flags> matrix_name(buffer_rows, rowsDataBuffer)
-
-  #define SMARTMATRIX_APA_ALLOCATE_BUFFERS(matrix_name, width, height, pwm_depth, buffer_rows, panel_type, option_flags) \
-      static DMAMEM SmartMatrixAPA102Refresh<pwm_depth, width, height, panel_type, option_flags>::frameDataStruct frameDataBuffer[buffer_rows]; \
-      SmartMatrixAPA102Refresh<pwm_depth, width, height, panel_type, option_flags> matrix_name##Refresh(buffer_rows, frameDataBuffer); \
-      SmartMatrixApaCalc<pwm_depth, width, height, panel_type, option_flags> matrix_name(buffer_rows, frameDataBuffer)
-
-#define SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(layer_name, width, height, storage_depth, scrolling_options) \
-  typedef RGB_TYPE(storage_depth) SM_RGB;                                                                 \
-  static uint8_t layer_name##Bitmap[width * (height / 8)];                                              \
-  static SMLayerScrolling<RGB_TYPE(storage_depth), scrolling_options> layer_name(layer_name##Bitmap, width, height)
-
-#define SMARTMATRIX_ALLOCATE_INDEXED_LAYER(layer_name, width, height, storage_depth, indexed_options) \
-  typedef RGB_TYPE(storage_depth) SM_RGB;                                                                 \
-  static uint8_t layer_name##Bitmap[2 * width * (height / 8)];                                              \
-  static SMLayerIndexed<RGB_TYPE(storage_depth), indexed_options> layer_name(layer_name##Bitmap, width, height)
-
-#define SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(layer_name, width, height, storage_depth, background_options) \
-  typedef RGB_TYPE(storage_depth) SM_RGB; \
-  static RGB_TYPE(storage_depth) layer_name##Bitmap[2 * width * height]; \
-  static color_chan_t layer_name##colorCorrectionLUT[sizeof(SM_RGB) <= 3 ? 256 : 4096];                          \
-  static SMLayerBackground<RGB_TYPE(storage_depth), background_options> layer_name(layer_name##Bitmap, width, height, layer_name##colorCorrectionLUT)
-
-#include "SmartMatrixPanelMaps.h" // include this to compile, but panel maps are not used or supported yet
-
-#include "SmartMatrixRefreshT4_Impl.h"
-#include "SmartMatrixCalcT4_Impl.h"
-
-#include "SmartMatrixAPA102RefreshTeensy4_Impl.h"
-#include "SmartMatrixAPA102Calc_Impl.h"
-
-#else // not Teensy 4.0/4.1
-
-#if defined(__arm__) && defined(CORE_TEENSY)
+#if defined(__arm__) && defined(CORE_TEENSY) && !defined(__IMXRT1062__) // Teensy 3.x
     #if defined(V4HEADER)
         #include "MatrixHardware_KitV4.h"
     #else
         #include "MatrixHardware_KitV1.h"
     #endif
+#endif
+
+#if defined(__IMXRT1062__) // Teensy 4.0/4.1
+  #include "MatrixHardware_KitV4T4.h"
 #endif
 
 #if defined(ESP32)
@@ -105,9 +67,14 @@
 
 #include "SmartMatrixPanelMaps.h"
 
-#if defined(__arm__) && defined(CORE_TEENSY)
+#if defined(__arm__) && defined(CORE_TEENSY) && !defined(__IMXRT1062__)  // Teensy 3.x
     #include "SmartMatrixMultiplexedRefreshTeensy.h"
     #include "SmartMatrixMultiplexedCalcTeensy.h"
+#endif
+
+#if defined(__IMXRT1062__) // Teensy 4.0/4.1
+    #include "SmartMatrixRefreshT4.h"
+    #include "SmartMatrixCalcT4.h"
 #endif
 
 #if defined(ESP32)
@@ -118,17 +85,25 @@
 #include "SmartMatrixAPA102Refresh.h"
 #include "SmartMatrixAPA102Calc.h"
 
-#if defined(__arm__) && defined(CORE_TEENSY)
+#if defined(__arm__) && defined(CORE_TEENSY) && !defined(__IMXRT1062__)  // Teensy 3.x
     #include "SmartMatrixCoprocessorSend.h"
     #include "SmartMatrixCoprocessorCalc.h"
 #endif
 
 #if defined(__arm__) && defined(CORE_TEENSY)
 #if 1
-    #define SMARTMATRIX_ALLOCATE_BUFFERS(matrix_name, width, height, pwm_depth, buffer_rows, panel_type, option_flags) \
-        static DMAMEM SmartMatrix3RefreshMultiplexed<pwm_depth, width, height, panel_type, option_flags>::rowDataStruct rowsDataBuffer[buffer_rows]; \
-        SmartMatrix3RefreshMultiplexed<pwm_depth, width, height, panel_type, option_flags> matrix_name##Refresh(buffer_rows, rowsDataBuffer); \
-        SmartMatrix3<pwm_depth, width, height, panel_type, option_flags> matrix_name(buffer_rows, rowsDataBuffer)
+    // TODO: use same definition for Teensy 3.x and 4.x HUB75 SMARTMATRIX_ALLOCATE_BUFFERS() if possible 
+    #if !defined(__IMXRT1062__) // Teensy 4.0/4.1
+        #define SMARTMATRIX_ALLOCATE_BUFFERS(matrix_name, width, height, pwm_depth, buffer_rows, panel_type, option_flags) \
+            static DMAMEM SmartMatrix3RefreshMultiplexed<pwm_depth, width, height, panel_type, option_flags>::rowDataStruct rowsDataBuffer[buffer_rows]; \
+            SmartMatrix3RefreshMultiplexed<pwm_depth, width, height, panel_type, option_flags> matrix_name##Refresh(buffer_rows, rowsDataBuffer); \
+            SmartMatrix3<pwm_depth, width, height, panel_type, option_flags> matrix_name(buffer_rows, rowsDataBuffer)
+    #else
+        #define SMARTMATRIX_ALLOCATE_BUFFERS(matrix_name, width, height, pwm_depth, buffer_rows, panel_type, option_flags) \
+            static volatile DMAMEM SmartMatrixRefreshT4<pwm_depth, width, height, panel_type, option_flags>::rowDataStruct rowsDataBuffer[buffer_rows]; \
+            SmartMatrixRefreshT4<pwm_depth, width, height, panel_type, option_flags> matrix_name##Refresh(buffer_rows, rowsDataBuffer); \
+            SmartMatrix3<pwm_depth, width, height, panel_type, option_flags> matrix_name(buffer_rows, rowsDataBuffer)
+    #endif
 
     #define SMARTMATRIX_APA_ALLOCATE_BUFFERS(matrix_name, width, height, pwm_depth, buffer_rows, panel_type, option_flags) \
         static DMAMEM SmartMatrixAPA102Refresh<pwm_depth, width, height, panel_type, option_flags>::frameDataStruct frameDataBuffer[buffer_rows]; \
@@ -178,10 +153,17 @@
 #endif
 
 // platform-specific
-#if defined(__arm__) && defined(CORE_TEENSY)
+#if defined(__arm__) && defined(CORE_TEENSY) && !defined(__IMXRT1062__)  // Teensy 3.x
     #include "SmartMatrixMultiplexedRefreshTeensy_Impl.h"
     #include "SmartMatrixAPA102RefreshTeensy_Impl.h"
     #include "SmartMatrixMultiplexedCalcTeensy_Impl.h"
+#endif
+
+#if defined(__IMXRT1062__) // Teensy 4.0/4.1
+    #include "SmartMatrixRefreshT4_Impl.h"
+    #include "SmartMatrixCalcT4_Impl.h"
+    #include "SmartMatrixAPA102RefreshTeensy4_Impl.h"
+    #include "SmartMatrixAPA102Calc_Impl.h"
 #endif
 
 #if defined(ESP32)
@@ -190,13 +172,10 @@
     #include "SmartMatrixMultiplexedCalcEsp32_Impl.h"
 #endif
 
-
-#if defined(__arm__) && defined(CORE_TEENSY)
+#if defined(__arm__) && defined(CORE_TEENSY) && !defined(__IMXRT1062__)  // Teensy 3.x
     #include "SmartMatrixCoprocessorSend_Impl.h"
     #include "SmartMatrixCoprocessorCalc_Impl.h"
     #include "SmartMatrixAPA102Calc_Impl.h"
-#endif
-
 #endif
 
 #endif
