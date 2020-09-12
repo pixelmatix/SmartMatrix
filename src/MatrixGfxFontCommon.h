d15ef3c7d53abc716abcfb5fa6ec0081a6c799a7
Louis Beaudoin <louis@embedded-creations.com>
2020-09-12 21:16:27 +0100

    Layer_BackgroundGFX - Add built in bdf2c fonts converted to Adafruit_GFX Font format

--- src/MatrixGfxFontCommon.h
+++ src/MatrixGfxFontCommon.h
@@ -5,6 +5,12 @@
 #include "MatrixFontCommon.h"
 
 // Adafruit_GFX fonts can be included only once, so include fonts used by the different GFX Layers in a central location
-#include <Fonts/Picopixel.h>
+#include "FontGFX_apple4x6.h"
+#include "FontGFX_apple5x7.h"
+#include "FontGFX_apple6x10.h"
+#include "FontGFX_apple8x13.h"
+#include "FontGFX_gohufont6x11.h"
+#include "FontGFX_gohufont6x11b.h"
+#include "FontGFX_tom_thumb.h"
 
 #endif
