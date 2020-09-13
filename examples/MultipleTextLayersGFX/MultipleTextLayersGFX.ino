07059d56d13b4fa0e80f0b866cf444be4c334ad4
Louis Beaudoin <louis@embedded-creations.com>
2020-09-13 23:13:56 +0100

    MultipleTextLayersGFX Example - Add optional FastLED pattern in background

--- examples/MultipleTextLayersGFX/MultipleTextLayersGFX.ino
+++ examples/MultipleTextLayersGFX/MultipleTextLayersGFX.ino
@@ -3,10 +3,16 @@
   This example requires the Adafruit_GFX Library, which you can install through Arduino Library Manager
   The "GFX" Layers in SmartMatrix Library 4.0 depend on Adafruit_GFX and give access to using large fonts included
     with Adafruit_GFX, and the ability to add custom fonts
-  Performance of the "GFX" layers has been optimized, and 10 scrolling text layers can be used with a 128x64 panel
+  Performance of the "GFX" layers has been optimized, and with the Teensy 4, you can have 10 scrolling text layers
+    on a 128x64 panel, and still refresh the panel at 240Hz
+  Optionally draw a pattern behind the text layers using FastLED (same code as the FastLED_Functions example) by
+    removing the comments from `#define INCLUDE_FASTLED_BACKGROUND` below.  With the Teensy 4 you'll still see 
+    128x64 @ 240Hz Refresh even with the pattern updating at 60 FPS!
 */
 
 #define SUPPORT_ADAFRUIT_GFX_LIBRARY
+
+//#define INCLUDE_FASTLED_BACKGROUND
 
 // uncomment one line to select your MatrixHardware configuration - configuration header needs to be included before <SmartMatrix3.h>
 //#include <MatrixHardware_ESP32_V0.h>    // This file contains multiple ESP32 hardware configurations, edit the file to define GPIOPINOUT (or add #define GPIOPINOUT with a hardcoded number before this #include)
@@ -38,6 +44,11 @@ const uint8_t kMatrixOptions = (SMARTMATRIX_OPTIONS_NONE);      // see http://do
 const uint8_t kScrollingLayerOptions = (SM_GFX_MONO_OPTIONS_NONE);
 
 SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
+
+#ifdef INCLUDE_FASTLED_BACKGROUND
+  const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_GFX_OPTIONS_NONE);
+  SMARTMATRIX_ALLOCATE_BACKGROUND_GFX_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
+#endif
 
 SMARTMATRIX_ALLOCATE_GFX_MONO_LAYER(scrollingLayer00, kMatrixWidth, kMatrixHeight, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
 SMARTMATRIX_ALLOCATE_GFX_MONO_LAYER(scrollingLayer01, kMatrixWidth, kMatrixHeight, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
@@ -88,10 +99,60 @@ rgb24 chooseRandomBrightColor() {
   return color;
 }
 
+#ifdef INCLUDE_FASTLED_BACKGROUND
+#include <FastLED.h>
+
+// The 32bit version of our coordinates
+static uint16_t x;
+static uint16_t y;
+static uint16_t z;
+
+// We're using the x/y dimensions to map to the x/y pixels on the matrix.  We'll
+// use the z-axis for "time".  speed determines how fast time moves forward.  Try
+// 1 for a very slow moving effect, or 60 for something that ends up looking like
+// water.
+// uint16_t speed = 1; // almost looks like a painting, moves very slowly
+uint16_t speed = 10; // a nice starting speed, mixes well with a scale of 100
+// uint16_t speed = 33;
+// uint16_t speed = 100; // wicked fast!
+
+// Scale determines how far apart the pixels in our noise matrix are.  Try
+// changing these values around to see how it affects the motion of the display.  The
+// higher the value of scale, the more "zoomed out" the noise iwll be.  A value
+// of 1 will be so zoomed in, you'll mostly see solid colors.
+
+// uint16_t scale = 1; // mostly just solid colors
+// uint16_t scale = 4011; // very zoomed out and shimmery
+uint16_t scale = 31;
+
+// This is the array that we keep our computed noise values in
+uint8_t noise[kMatrixWidth][kMatrixHeight];
+
+// Fill the x/y array of 8-bit noise values using the inoise8 function.
+void fillnoise8() {
+  for(int i = 0; i < kMatrixWidth; i++) {
+    int ioffset = scale * i;
+    for(int j = 0; j < kMatrixHeight; j++) {
+      int joffset = scale * j;
+      noise[i][j] = inoise8(x + ioffset,y + joffset,z);
+    }
+  }
+  z += speed;
+}
+
+#endif
+
 void setup() {
   delay(2000);
 
   randomSeed(analogRead(0));
+#ifdef INCLUDE_FASTLED_BACKGROUND
+  matrix.addLayer(&backgroundLayer); 
+  // Initialize our coordinates to some random values
+  x = random16();
+  y = random16();
+  z = random16();
+#endif
 
   matrix.addLayer(&scrollingLayer00); 
   matrix.addLayer(&scrollingLayer01); 
@@ -185,6 +246,49 @@ void setup() {
 }
 
 void loop() {
+#ifdef INCLUDE_FASTLED_BACKGROUND
+  
+  static uint8_t circlex = 0;
+  static uint8_t circley = 0;
+
+  // if sketch uses swapBuffers(false), wait to get a new backBuffer() pointer after the swap is done:
+  while(backgroundLayer.isSwapPending());
+
+  rgb24 *buffer = backgroundLayer.backBuffer();
+
+  static uint8_t ihue=0;
+  fillnoise8();
+  for(int i = 0; i < kMatrixWidth; i++) {
+    for(int j = 0; j < kMatrixHeight; j++) {
+      // We use the value at the (i,j) coordinate in the noise
+      // array for our brightness, and the flipped value from (j,i)
+      // for our pixel's hue.
+      CRGB temprgb = CRGB(CHSV(noise[j][i],255,noise[i][j]));
+      buffer[kMatrixWidth*j + i] = rgb24(temprgb.r, temprgb.g, temprgb.b);
+
+      // You can also explore other ways to constrain the hue used, like below
+      // buffer[kMatrixHeight*j + i] = CRGB(CHSV(ihue + (noise[j][i]>>2),255,noise[i][j]));
+    }
+  }
+  ihue+=1;
+
+  //backgroundLayer.fillCircle(circlex % kMatrixWidth,circley % kMatrixHeight,6,CRGB(CHSV(ihue+128,255,255)));
+  circlex += random16(2);
+  circley += random16(2);
+
+  // buffer is filled completely each time, use swapBuffers without buffer copy to save CPU cycles
+  backgroundLayer.swapBuffers(false);
+  matrix.countFPS();      // print the loop() frames per second to Serial
+  EVERY_N_SECONDS( 1 ) {   
+    Serial.print("Refresh Rate: ");
+    Serial.println(matrix.getRefreshRate()); 
+  }
+
+#else  
+
+  Serial.print("Refresh Rate: ");
   Serial.println(matrix.getRefreshRate());
   delay(1000);
+
+#endif
 }
