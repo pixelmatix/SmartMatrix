6f54729bfdedf4c3e5bef2a4dcba91fc0628780c
Louis Beaudoin <louis@embedded-creations.com>
2020-11-30 17:38:05 +0000

    update error message

--- src/SmartLEDShieldV4.h
+++ src/SmartLEDShieldV4.h
@@ -1,1 +1,1 @@
-#pragma GCC error "SmartLEDShieldV4.h is deprecated, use #include <MatrixHardware_Teensy3_ShieldV4.h> at the top of your sketch instead"
+#pragma GCC error "SmartLEDShieldV4.h is isn't supported as of SmartMatrix Library 4.0, use #include <MatrixHardware_Teensy3_ShieldV4.h> at the top of your sketch instead"
