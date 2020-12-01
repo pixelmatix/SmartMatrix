1c6e53c8b4317419b4b56ec49235e9ffb92581db
Louis Beaudoin <louis@embedded-creations.com>
2020-12-01 16:48:25 +0000

    Update to 4.0.3 and rename to SmartMatrix from SmartMatrix3

--- src/SmartMatrix.h
+++ src/SmartMatrix.h
@@ -34,12 +34,12 @@
 #define SM_HAS_PRAGMA_MESSAGE
 #endif
 
-#define SM_VERSION 4000002
+#define SM_VERSION 4000003
 #ifndef SM_INTERNAL
 #  ifdef SM_HAS_PRAGMA_MESSAGE
-#    pragma message "SmartMatrix Library version 4.000.002"
+#    pragma message "SmartMatrix Library version 4.000.003"
 #  else
-#    warning SmartMatrix Library version 4.000.002  (Not really a warning, just telling you here.)
+#    warning SmartMatrix Library version 4.000.003  (Not really a warning, just telling you here.)
 #  endif
 #endif
 
