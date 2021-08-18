c8fcb0b06bf31d0b2c2dba37b3c5b3ff1b5d1407
Francesco Capano <fcapano@gmail.com>
2021-08-18 14:18:03 +0100

    Fix missing includes to build as esp-idf component

--- src/esp32_i2s_parallel.c
+++ src/esp32_i2s_parallel.c
@@ -26,8 +26,10 @@
 
 #include "soc/i2s_struct.h"
 #include "soc/i2s_reg.h"
+#include "driver/gpio.h"
 #include "driver/periph_ctrl.h"
 #include "soc/io_mux_reg.h"
+#include "soc/gpio_periph.h"
 #include "rom/lldesc.h"
 #include "esp_heap_caps.h"
 #include "esp32_i2s_parallel.h"
