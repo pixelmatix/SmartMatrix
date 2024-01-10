a4e26adf50a87b2df44bf2998310bfb91e0cd33e
Franz Höpfinger <mail@franzhoepfinger.de>
2024-01-10 11:22:42 +0100

    gitflow-feature-stash: ELECTRODRAGON_PINOUT

--- src/MatrixHardware_ESP32_V0.h
+++ src/MatrixHardware_ESP32_V0.h
@@ -39,9 +39,10 @@
 #define ESP32_JC_RIBBON_PINOUT_WEMOS    8
 #define HUB75_ADAPTER_LITE_V0_PINOUT    9
 #define ESP32_RGB64x32MatrixPanel_I2S_DMA_DEFAULT    10
+#define ELECTRODRAGON_PINOUT    11
 
 #ifndef GPIOPINOUT
-#define GPIOPINOUT ESP32_FORUM_PINOUT
+#define GPIOPINOUT ELECTRODRAGON_PINOUT
 #endif
 //#define GPIOPINOUT ESP32_FORUM_PINOUT_WITH_LATCH // note this mode is untested as of 2018-05-17 - not being used anymore now that SmartMatrix Shield is available
 //#define GPIOPINOUT SMARTLED_SHIELD_V0_PINOUT
@@ -248,6 +249,68 @@
     15	OE	25	LED Drivers' Output Enable
     16	GND	GND	Ground
     */ 
+    #define R1_PIN  GPIO_NUM_2
+    #define G1_PIN  GPIO_NUM_15
+    #define B1_PIN  GPIO_NUM_4
+    #define R2_PIN  GPIO_NUM_16
+    #define G2_PIN  GPIO_NUM_27
+    #define B2_PIN  GPIO_NUM_17
+
+    #define A_PIN   GPIO_NUM_5
+    #define B_PIN   GPIO_NUM_18
+    #define C_PIN   GPIO_NUM_19
+    #define D_PIN   GPIO_NUM_21
+    #define E_PIN   GPIO_NUM_12
+    #define LAT_PIN GPIO_NUM_26
+    #define OE_PIN  GPIO_NUM_25
+
+    #define CLK_PIN GPIO_NUM_22
+
+#elif (GPIOPINOUT == ELECTRODRAGON_PINOUT)
+
+    #pragma message "MatrixHardware: ESP32 forum wiring"
+
+    // ADDX is output directly using GPIO
+    #define CLKS_DURING_LATCH   0
+    #define MATRIX_I2S_MODE I2S_PARALLEL_BITS_16
+    #define MATRIX_DATA_STORAGE_TYPE uint16_t
+
+    /*
+    HUB 75
+    01 02 B0
+    03 04 Gnd
+    05 06 G1
+    07 08 E
+
+    09 10 B
+    11 12 D
+    13 14 STB/Latch
+    15 16 Gnd
+                        ESP32 pin / comment
+    1	R0	2	Red Data (columns 1-16)
+    2	G0	15	Green Data (columns 1-16)
+
+    3	B0	4	Blue Data (columns 1-16)
+    4	GND	GND	Ground
+
+    5	R1	16/RX2	Red Data (columns 17-32)
+    6	G1	27	Green Data (columns 17-32)
+
+    7	B1	17/TX2	Blue Data (columns 17-32)
+    8	E	12	Demux Input E for 64x64 panels
+
+    9	A	5	Demux Input A0
+    10	B	18	Demux Input A1
+
+    11	C	19	Demux Input A2
+    12	D	21	Demux Input E1, E3 (32x32 panels only)
+
+    13	CLK	22	LED Drivers' Clock
+    14	STB	26	LED Drivers' Latch
+
+    15	OE	25	LED Drivers' Output Enable
+    16	GND	GND	Ground
+    */
     #define R1_PIN  GPIO_NUM_2
     #define G1_PIN  GPIO_NUM_15
     #define B1_PIN  GPIO_NUM_4
